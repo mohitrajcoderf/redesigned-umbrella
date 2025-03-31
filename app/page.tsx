5b0fb12aaad24d9312f68f77ab0005a1b4f37a7a
mohitrajcoderf <161021797+mohitrajcoderf@users.noreply.github.com>
2025-03-31 12:27:39 +0530

    next app init
    basic app demo

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button className="bg-amber-950 hover:bg-amber-600">mohit raj</Button>
+    </div>
+  )
+}
